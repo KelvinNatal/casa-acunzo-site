c9fd3ab7063aa8d59a32a250012421cdf1727a26
KelvinNatal <kelvinnatal@hotmail.com>
2023-12-04 23:10:48 -0300

    Adicionando metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='scroll-smooth'>
-      {/* <head>
-        <link rel='shortcut icon' href='./favicon.ico'/>
-      </head> */}
+    <html lang="pt-br" className='scroll-smooth'>
+      <head>
+        <meta name="title" content="Casa Acunzo"/>
+        <meta name="description" content="Desfrute de uma experiência sensorial incomparável em nossa casa de vinhos, explore uma seleção cuidadosamente escolhida de rótulos de qualidade."/>
+        <meta name="keywords" content="vinhos, vinicolas, Casa Acunzo, comprar vinho, experiencia, bebidas"/>
+        <meta name="robots" content="index, follow"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta name="language" content="Portuguese"/>
+        <meta name="revisit-after" content="1 days"/>
+      </head>
       <body className={tenor.className}>{children}</body>
     </html>
   )
